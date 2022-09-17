4db0a13ce17066154350a1bc4d56987c30662d9d
Eunice Igbinedion <eunice.igbinedion@yahoo.com>
2022-09-17 08:38:29 +0200

    Updating Styles for Home Component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -12,14 +12,14 @@ const Home = () => {
         </form>
       </div>
       <div className="search-results">
-        <div className="User">
+        <div className="user">
           <div className="image">
-          <img src='' alt="" />
+          <img src='https://scontent-frt3-1.xx.fbcdn.net/v/t1.6435-9/185872471_10215171635523483_1904449185798158987_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=G9o73-7NUBgAX-d0DCr&_nc_oc=AQnhIYyj6AV506JCkX-WQMqhNyYsyNu5uneln_s1CUO9h2_GZAJnzTNFV9PQGuRMnzY&tn=6n4Q6I1YmwmBo1l1&_nc_ht=scontent-frt3-1.xx&oh=00_AT_mrBovunTS_QpR-KtHSZgcWldklzchmHFl_bGbYHN0hw&oe=634915C2' alt="" />
           </div>
           <div className='user-info'>
-          <h4>Name of User</h4>
-          <small>ID</small>
-          <a href=''>View profile</a>
+          <h3>Name of User</h3>
+          <small>ID532kl: </small>
+          <a href="">View profile</a>
           </div>
         </div>
       </div>
