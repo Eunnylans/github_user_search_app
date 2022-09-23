684f6566880d263f5381ef85264a1b467219d84e
Eunice Igbinedion <eunice.igbinedion@yahoo.com>
2022-09-23 11:52:25 +0200

    Updating Home and User Component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,44 @@
-import React, { useState } from "react";
-import axios from '../../axios';
+import React, { useEffect, useState } from "react";
+import axios from "../../axios";
+import User from "../../components/UserDetails/User";
 import "./Home.css";
-
 const Home = () => {
-
   const [query, setQuery] = useState("");
+  //Users fetched from the API
   const [users, setUsers] = useState([]);
+  //Page
+  const [page, setPage] = useState(1);
+  //Per page
+  const [limit, setLimit] = useState(10);
 
-  const handleQueryInput = (e) =>{
+  const handleQueryInput = (e) => {
     const value = e.target.value;
     setQuery(value);
-  }
+  };
+  const handlePrevPage = () => {
+    setPage((page) => {
+      if (page === 1) return page;
+      else return page - 1;
+    });
+  };
+
+  const handleNextPage = () => {
+    setPage((page) => page + 1);
+  };
+
+  const handlePageLimit = (e) => {
+    const value = e.target.value;
+    setLimit(parseInt(value));
+  };
 
   const fetchUsers = async () => {
     try {
-      const { data } = await axios.get("/search/users?q=" + query);
+      const { data } = await axios.get("/search/users?q=" + query, {
+        params: {
+          page,
+          per_page: limit,
+        },
+      });
       return data?.items;
     } catch (error) {
       console.error(error);
@@ -22,16 +46,25 @@ const Home = () => {
     }
   };
 
-  const handleSearchUsers = (e) =>{
+  const handleSearchUsers = async (e) => {
     e.preventDefault();
-    if(query) {
+    if (query) {
       const items = await fetchUsers();
       setUsers(items);
-    }
-    else {
+    } else {
       console.log("Your query is empty...");
     }
-  }
+  };
+
+  useEffect(() => {
+    const displayUsersOnChange = async () => {
+      if (query) {
+        const items = await fetchUsers();
+        setUsers(items);
+      }
+    };
+    displayUsersOnChange();
+  }, [page, limit]);
 
   return (
     <div className="container">
@@ -43,11 +76,28 @@ const Home = () => {
         </form>
       </div>
       <div className="search-results">
-      <div className="more-options"></div>
-
-      {users ? users.map(user =>{
-        return <User user={user} key={user.id} />
-      }) :  <h2>There is nothing to display</h2>}
+        <div className="more-options">
+          <label>
+            <small>Per Page</small>
+            <select onChange={handlePageLimit}>
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
+            </select>
+          </label>
+          <div className="pagination">
+            <button onClick={handlePrevPage}>{page}</button>
+            <button onClick={handleNextPage}>{page + 1}</button>
+          </div>
+        </div>
+        {users ? (
+          users.map((user) => {
+            return <User user={user} key={user.id} />;
+          })
+        ) : (
+          <h2>There is nothing to display...</h2>
+        )}
       </div>
     </div>
   );
